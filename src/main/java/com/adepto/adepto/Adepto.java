13eddfec5ecf82ce41d1334d5369f3ddbaee2b8a
Criticyl <criticyl@gmail.com>
2021-12-12 20:19:05 +0000

    Added Hello World

--- src/main/java/com/adepto/adepto/Adepto.java
+++ src/main/java/com/adepto/adepto/Adepto.java
@@ -7,7 +7,7 @@ public final class Adepto extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Hello World");
     }
 
     @Override
